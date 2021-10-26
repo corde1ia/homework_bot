6f2a809edc66722f6ad72ef55ae2304f95438853
Natalya <ngkolyadina@1367.ru>
2021-10-26 12:31:48 +0300

    second attemp

--- homework.py
+++ homework.py
@@ -1,6 +1,3 @@
-# мне очень жаль Ваши сломанные глаза
-# очевидно, что я мало понимаю, что делаю
-
 import logging
 import os
 import time
@@ -18,12 +15,15 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(filename)s - %(message)s',
 )
 
-PRACTICUM_TOKEN = os.getenv('TOKEN_PRACTIKUM')
-TELEGRAM_TOKEN = os.getenv('TOKEN')
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 
 RETRY_TIME = 300
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+
+
+HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -32,37 +32,40 @@ HOMEWORK_STATUSES = {
 }
 
 
-def get_api_answer(api_url, current_timestamp):
+def get_api_answer(url, current_timestamp):
     """Обращается к API Практикум.Домашка.
     Получает даннные о последней отправленной на ревью работы.
     """
     current_timestamp = current_timestamp or int(time.time())
-    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    headers = HEADERS
     payload = {'from_date': current_timestamp}
-    api_url = ENDPOINT
     response = requests.get(
-        api_url,
+        ENDPOINT,
         headers=headers,
         params=payload)
     if response.status_code != 200:
         raise Exception('Invalid server respond')
-    logging.error('Ответ сервера не совпал с ожидаемым')
+    logging.error('Unexpected server response')
     return response.json()
 
 
 def check_response(response):
     """Проверяет содержимое ответа после запроса к API."""
-    homeworks = response.get('homeworks')
-    if homeworks is None:
-        raise Exception('Invalid response: response is empty')
-    logging.info('Полученный ответ не содержит данных.')
-    for homework in homeworks:
-        status = homework.get('status')
-        if status in HOMEWORK_STATUSES.keys():
-            return homeworks
+    # проверку на пустой список пришлось ввести,
+    # потому что иначе при выставлении int(time.time())
+    # вылезала ошибка 'index out of range' для response['homeworks'][0]
+    # так как если статус не изменился, я получала [] в homework
+    homework = response.get('homeworks')
+    if homework != []:
+        homework = response['homeworks'][0]
+    else:
+        return homework
+    status = homework['status']
+    if status in HOMEWORK_STATUSES:
+        return homework
+    else:
         raise Exception('Invalid rewiew status')
-    logging.info('Полученный статус неизвестен.')
-    return homeworks
+    logging.info('Invalid rewiew status')
 
 
 def parse_status(homework):
@@ -70,35 +73,32 @@ def parse_status(homework):
     Возвращает ответ в зависимости от полученного статуса.
     """
     homework_name = homework.get('homework_name')
-    verdict = HOMEWORK_STATUSES.get(homework.get('status'))
-    if homework_name is None:
-        raise Exception("No homework name")
-    logging.error('В полученном ответе не содержится '
-                  'названия работы.')
-    if verdict is None:
-        raise Exception("No verdict")
-    logging.error('В полученном ответе не соержится '
-                  'статус работы.')
-    for status in verdict:
-        if status == verdict:
-            verdict = HOMEWORK_STATUSES.get(homework.get('status'))
+    verdict = HOMEWORK_STATUSES[homework['status']]
+    if homework_name is None or verdict is None:
+        raise Exception("Invalid results")
+    logging.error('Result does not '
+                  'contains any homework names '
+                  'or verdicts')
     return f'Изменился статус проверки работы "{homework_name}".\n\n{verdict}'
 
 
 def send_message(bot, message):
     """Обращается к API Телеграмма и после отправляет Богу сообщение."""
-    logging.info('Сообщение успешно отправлено')
-    return bot.send_message(chat_id=CHAT_ID, text=message)
+    logging.info('Message was successfully sent')
+    try:
+        bot.send_message(chat_id=CHAT_ID, text=message)
+    except Exception:
+        logging.error('Something went wrong. '
+                      'Bot is unavalible')
 
 
 def main():
     """Управляет логикой работы бота."""
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    url = ENDPOINT
     while True:
         try:
-            get_api_answer_main = get_api_answer(url, current_timestamp)
+            get_api_answer_main = get_api_answer(ENDPOINT, current_timestamp)
             check_response_main = check_response(get_api_answer_main)
             if check_response_main:
                 for homework in check_response_main:
@@ -106,13 +106,13 @@ def main():
                     send_message(bot, parse_status_result)
             time.sleep(RETRY_TIME)
         except Exception:
-            logging.error('Что-то пошло не так, Бот недоступен.')
+            logging.error('Something went wrong. '
+                          'Bot is unavalible')
             bot.send_message(
                 chat_id=CHAT_ID,
-                text='Что-то пошло не так.'
+                text='HELP ME! Something IS wrong'
             )
             time.sleep(RETRY_TIME)
-            continue
 
 
 if __name__ == '__main__':
