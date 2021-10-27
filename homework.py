e551357de26eeb51fafa5492857a52dbd03c9836
Natalya <ngkolyadina@1367.ru>
2021-10-27 11:25:18 +0300

    3 attempt

--- homework.py
+++ homework.py
@@ -22,7 +22,6 @@ CHAT_ID = os.getenv('CHAT_ID')
 RETRY_TIME = 300
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 
-
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 HOMEWORK_STATUSES = {
@@ -34,38 +33,32 @@ HOMEWORK_STATUSES = {
 
 def get_api_answer(url, current_timestamp):
     """Обращается к API Практикум.Домашка.
-    Получает даннные о последней отправленной на ревью работы.
+    Получает даннные о последней отправленной на ревью работе.
     """
     current_timestamp = current_timestamp or int(time.time())
-    headers = HEADERS
     payload = {'from_date': current_timestamp}
     response = requests.get(
         ENDPOINT,
-        headers=headers,
+        headers=HEADERS,
         params=payload)
     if response.status_code != 200:
-        raise Exception('Invalid server respond')
-    logging.error('Unexpected server response')
+        logging.error('Unexpected server response', exc_info=True)
+        raise Exception('Invalid rewiew status')
     return response.json()
 
 
 def check_response(response):
     """Проверяет содержимое ответа после запроса к API."""
-    # проверку на пустой список пришлось ввести,
-    # потому что иначе при выставлении int(time.time())
-    # вылезала ошибка 'index out of range' для response['homeworks'][0]
-    # так как если статус не изменился, я получала [] в homework
-    homework = response.get('homeworks')
-    if homework != []:
-        homework = response['homeworks'][0]
-    else:
-        return homework
+    homeworks = response.get('homeworks')
+    if not homeworks:
+        logging.error('Response is empty', exc_info=True)
+        raise Exception('Invalid response')
+    homework = homeworks[0]
     status = homework['status']
     if status in HOMEWORK_STATUSES:
         return homework
-    else:
-        raise Exception('Invalid rewiew status')
-    logging.info('Invalid rewiew status')
+    logging.error('Invalid rewiew status', exc_info=True)
+    raise Exception('Invalid rewiew status')
 
 
 def parse_status(homework):
@@ -74,22 +67,23 @@ def parse_status(homework):
     """
     homework_name = homework.get('homework_name')
     verdict = HOMEWORK_STATUSES[homework['status']]
-    if homework_name is None or verdict is None:
+    if homework_name is None:
+        logging.error('Result does not '
+                      'contains any homework names',
+                      exc_info=True)
         raise Exception("Invalid results")
-    logging.error('Result does not '
-                  'contains any homework names '
-                  'or verdicts')
     return f'Изменился статус проверки работы "{homework_name}".\n\n{verdict}'
 
 
 def send_message(bot, message):
-    """Обращается к API Телеграмма и после отправляет Богу сообщение."""
+    """Обращается к API Телеграмма и после отправляет Боту сообщение."""
     logging.info('Message was successfully sent')
     try:
         bot.send_message(chat_id=CHAT_ID, text=message)
     except Exception:
         logging.error('Something went wrong. '
-                      'Bot is unavalible')
+                      'Bot is unavalible',
+                      exc_info=True)
 
 
 def main():
@@ -101,16 +95,16 @@ def main():
             get_api_answer_main = get_api_answer(ENDPOINT, current_timestamp)
             check_response_main = check_response(get_api_answer_main)
             if check_response_main:
-                for homework in check_response_main:
-                    parse_status_result = parse_status(homework)
-                    send_message(bot, parse_status_result)
+                parse_status_result = parse_status(check_response_main)
+                send_message(bot, parse_status_result)
             time.sleep(RETRY_TIME)
         except Exception:
             logging.error('Something went wrong. '
-                          'Bot is unavalible')
+                          'Bot is unavalible',
+                          exc_info=True)
             bot.send_message(
                 chat_id=CHAT_ID,
-                text='HELP ME! Something IS wrong'
+                text='HELP ME! Something went wrong'
             )
             time.sleep(RETRY_TIME)
 
